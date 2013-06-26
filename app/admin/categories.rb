c0bd16e69792e4854676c0eb240c2d3071279bad
Mauro Morales <contact@mauromorales.com>
2013-06-26 10:21:21 +0200

    list items inside categories

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -2,4 +2,25 @@ ActiveAdmin.register Category do
   
   menu :parent => "Inventory"
 
+  show do |category|
+    attributes_table do
+      row :id
+      row :name
+    end
+
+    columns do
+      column do
+        panel "Items" do
+          table_for category.items  do 
+            column :name do |item|
+              link_to item.name, admin_item_path(item)
+            end
+            column :brand
+            column :units
+          end
+        end
+      end
+    end
+
+  end
 end
