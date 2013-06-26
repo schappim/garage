befcc7ff845d9770b19f6dd701dc18dd5dc14641
Mauro Morales <contact@mauromorales.com>
2013-06-26 12:24:45 +0200

    shouldn't be able to edit purchases or withdrawals

--- app/admin/purchases.rb
+++ app/admin/purchases.rb
@@ -3,4 +3,29 @@ ActiveAdmin.register Purchase do
   menu :parent => "Inventory"
 
   actions :all, :except => [:destroy]
+
+  index  do
+    column :sku do |purchase|
+      link_to purchase.item.sku, admin_item_path(purchase.item)
+    end
+    column :item do |purchase|
+      purchase.item.name
+    end
+    column :units
+    column :cost_per_unit
+    column :total_cost
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :item
+      if f.object.new_record?
+        f.input :units
+      end
+      f.input :cost_per_unit
+      f.input :total_cost
+    end
+    f.actions
+  end
 end
--- app/admin/withdrawals.rb
+++ app/admin/withdrawals.rb
@@ -4,4 +4,26 @@ ActiveAdmin.register Withdrawal do
 
   actions :all, :except => [:destroy]
 
+  index  do
+    column :sku do |purchase|
+      link_to purchase.item.sku, admin_item_path(purchase.item)
+    end
+    column :item do |purchase|
+      purchase.item.name
+    end
+    column :units
+    column :reason
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :item
+      if f.object.new_record?
+        f.input :units
+      end
+      f.input :reason
+    end
+    f.actions
+  end
 end
